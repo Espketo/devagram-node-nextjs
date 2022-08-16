87e3f9124c00c8dd6913a775ae3e2ea1011e412b
Samuel Rodrigues <rodrigues.samuel98@gmail.com>
2022-08-16 15:39:10 -0300

    versao inicial do login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -15,4 +15,4 @@ export default (
         return res.status(400).json({erro : 'Usuário ou senha não encontrado'});
     }    
     return res.status(405).json({erro : 'Metodo informado não é valido'});
-}
+}   
