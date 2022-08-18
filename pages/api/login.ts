04a219eb35a22cc9af52f53d572972ae6926a93e
Samuel Rodrigues <rodrigues.samuel98@gmail.com>
2022-08-18 15:02:17 -0300

    conclusão da matéria de Schemas

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModels'; 
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' && 
-            senha === 'admin@123'){
-                return res.status(200).json({msg: 'Usuário autenticado com sucesso'});
+        const usuariosEncontrado = await UsuarioModel.find({email:login, senha:md5(senha)});
+
+        if(usuariosEncontrado && usuariosEncontrado.length > 0){
+                const usuarioEncontrado = usuariosEncontrado[0];
+                return res.status(200).json({msg: `Usuário ${usuarioEncontrado.nome} autenticado com sucesso`});
         }
         return res.status(400).json({erro : 'Usuário ou senha não encontrado'});
     }    
