11c0d6a531feeb28ab2ce253173b7f941421759f
Samuel Rodrigues <rodrigues.samuel98@gmail.com>
2022-08-23 14:46:08 -0300

    integração Cosmic e Multer

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,47 +2,59 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
 import {UsuarioModel} from '../../models/UsuarioModels';
-import {conectarMongoDB } from '../../middlewares/conectarMongoDB';
-
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import md5 from 'md5';
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
+import nc from 'next-connect';
 
-const endpointCadastro =  
-    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) =>{
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+        try{
+            const usuario = req.body as CadastroRequisicao;
         
-    if(req.method === 'POST') 
-    {
-        const usuario = req.body as CadastroRequisicao;
+            if(!usuario.nome || usuario.nome.length < 2){
+                return res.status(400).json({erro : 'Nome invalido'});
+            }
+    
+            if(!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({erro : 'Email invalido'});
+            }
+    
+            if(!usuario.senha || usuario.senha.length < 4){
+                return res.status(400).json({erro : 'Senha invalida'});
+            }
+    
+            // validacao se ja existe usuario com o mesmo email
+            const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+            if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+                return res.status(400).json({erro : 'Ja existe uma conta com o email informado'});
+            }
 
-        if(!usuario.nome || usuario.nome.length < 2){
-            return res.status(400).json({erro : 'Nome invalido'});
+            // enviar a imagem do multer para o cosmic
+            const image = await uploadImagemCosmic(req);
+    
+            // salvar no banco de dados
+            const usuarioASerSalvo = {
+                nome : usuario.nome,
+                email : usuario.email,
+                senha : md5(usuario.senha),
+                avatar : image?.media?.url
+            }
+            await UsuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({msg : 'Usuario criado com sucesso'});
+        }catch(e : any){
+            console.log(e);
+            return res.status(400).json({erro : e.toString()});
         }
+});
 
-        if(!usuario.email || usuario.email.length < 5 
-            || !usuario.email.includes('@') 
-            || !usuario.email.includes('.')){
-            return res.status(400).json({erro : 'E-mail invalido'});
-        }
-
-        if(!usuario.senha || usuario.senha.length <4){
-            return res.status(400).json({erro : 'Senha invalido'});
-        }
-
-        //validacao se ja existe usuario com o mesmo email
-        const UsuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
-        if(UsuariosComMesmoEmail && UsuariosComMesmoEmail.length > 0) {
-            return res.status(400).json({erro : 'Já existe uma conta com o email informado'});
-        }
-
-        //salvar no bando de dados
-        const usuarioASerSalvo = {
-            nome: usuario.nome,
-            email: usuario.email,
-            senha: md5(usuario.senha),
-        }
-        await UsuarioModel.create(usuarioASerSalvo);
-        return res.status(200).json({msg : 'Usuario criado com sucesso'});
+export const config = {
+    api: {
+        bodyParser : false
     }
-    return res.status(405).json({erro: 'Metodo informado não e valido'});
 }
 
-export default conectarMongoDB(endpointCadastro);
+export default conectarMongoDB(handler);
